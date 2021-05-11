24f4317a9ce5e83f6d87fbf57eae06d90b7492f2
omkarhcode <omkarh.code@gmail.com>
2021-05-11 18:05:28 +0530

    New Django project myClub includs events users and more.

---
+++ events/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+# Create your views here.
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	name = "Omkar"
+
+	month = month.capitalize()
+	# convert month from name to number
+	month_number = list(calendar.month_name).index(month)	
+	month_number = int(month_number)
+
+	# Create a calendar
+	cal = HTMLCalendar().formatmonth(year, month_number)
+
+	return render(request, 'home.html',
+		{
+		"name": name,
+		"year": year,
+		"month": month, 
+		"month_number": month_number,
+		"cal": cal,
+		})
