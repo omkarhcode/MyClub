d88daa336494b51c7c9891c5166d78f345dd7694
omkarhcode <omkarh.code@gmail.com>
2021-05-13 20:16:25 +0530

    added Update event functionality

--- events/urls.py
+++ events/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('list_venues', views.list_venues, name="list-venues"),
     path('show_venue/<venue_id>', views.show_venue, name="show-venue"),
     path('update_venue/<venue_id>', views.update_venue, name="update-venue"),
+    path('update_event/<event_id>', views.update_event, name="update-event"),
     path('search_venues', views.search_venues, name="search-venues"),
     path('add_event', views.add_event, name="add-event"),
 ]
