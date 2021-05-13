f715b98f53f1059400dae9821be881814c2097e5
omkarhcode <omkarh.code@gmail.com>
2021-05-13 20:33:27 +0530

    Delete event and venue functionality added.

--- events/views.py
+++ events/views.py
@@ -6,7 +6,15 @@ from django.http import HttpResponseRedirect
 from .models import Event, Venue
 from .forms import VenueForm, EventForm
 
+def delete_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	venue.delete()
+	return redirect('list-venues')
 
+def delete_event(request, event_id):
+	event = Event.objects.get(pk=event_id)
+	event.delete()
+	return redirect('list-events')
 
 def update_event(request, event_id):
 	event = Event.objects.get(pk=event_id)
