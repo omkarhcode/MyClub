c7ed9f75300e921a8d9159e38ae064ee22b3128c
omkarhcode <omkarh.code@gmail.com>
2021-07-02 13:08:35 +0530

    Added login page

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -4,9 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members', include( 'django.contrib.auth.urls')),
+    path('members', include('members.urls')),
 ]
 
 # Configure Admin titles
 admin.site.site_header = "My Club Administration"
 admin.site.site_title = "Admin"
-admin.site.index_title = "Welcome to Admin "
+admin.site.index_title = "Welcome to Admin"
